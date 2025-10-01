142c1f44c8befe1eda809c0d531ef359f38cb6f8
sagar kamankar <sagarkamankar26@gmail.com>
2025-10-01 11:05:53 +0530

    add more routes and test cases

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,3 +5,11 @@ def test_home():
     resp=client.get("/")
     assert resp.status_code==200
     assert resp.data == b"Hello World !"
+
+
+
+def test_health():
+    client=app.test_client()
+    resp=client.get("/health")
+    assert resp.status_code==200
+    assert resp.json=={"status":"ok"}    
