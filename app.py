d98bd5b8a6b145cacc01d03aba607b4d24d06ca9
sagar kamankar <sagarkamankar26@gmail.com>
2025-10-01 14:53:46 +0530

    Support PORT env for Render

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask,jsonify,request
+from flask import Flask,jsonify,request,abort
+import os
 
 app=Flask(__name__)
 
@@ -25,4 +26,5 @@ def cal_sum():
 
 
 if __name__=="__main__":
-    app.run(debug=True,host="0.0.0.0",port=5000)
+    port=int(os.environ.get("PORT",8000))
+    app.run(debug=True,host="0.0.0.0",port=port)
