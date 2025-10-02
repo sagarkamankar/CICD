c196624583831b91b9385831cb67ff95d49c23ff
sagarkamankar <sagarkamankar1920@gmail.com>
2025-10-02 21:02:02 +0530

    changes in use cases

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,7 +12,7 @@ def test_health():
     client=app.test_client()
     resp=client.get("/health")
     assert resp.status_code==200
-    assert resp.json=={"status":"ok"}    
+    assert resp.json=={"status":"sagar"}    
 
 def test_echo():
     client=app.test_client()
